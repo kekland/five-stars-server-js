86817a52ff7fa4cd306a4535a95d3334ab8d158d
kekland <kk.erzhan@gmail.com>
2019-05-31 10:24:29 +0600

    added cargo PUT request

--- src/controllers/cargo.controller.ts
+++ src/controllers/cargo.controller.ts
@@ -6,21 +6,41 @@ import { validate } from 'class-validator';
 import { ValidationService } from '../lapis_server/utils';
 import { Cargo } from '../models/cargo.model';
 import { BadRequestException } from '../lapis_server/errors';
-import { Get, Post } from '../lapis_server/request.methods';
+import { Request } from 'express'
+import { Get, Post, Put } from '../lapis_server/request.methods';
 
 @RoutedController('/cargo')
 export class CargoController extends Controller {
-  @Get()
+  @Get('/')
   async getAll(req) {
     const data = await DatabaseService.cargoStore.get().run()
     return data
   }
 
-  @Post()
+  @Post('/')
   async create(req) {
     const data = await ValidationService
-                       .transformAndValidate<CargoAssignRequestObject>(req.body, () => CargoAssignRequestObject)
+      .transformAndValidate<CargoAssignRequestObject>(req.body, () => CargoAssignRequestObject)
     const cargo = await DatabaseService.cargoStore.push().item(Cargo.fromAssignRequestObject(data, 'kekland')).run()
     return cargo
+  }
+
+  @Put('/:id')
+  async update(req: Request) {
+    const data = await ValidationService
+      .transformAndValidate<CargoAssignRequestObject>(req.body, () => CargoAssignRequestObject)
+
+    const cargo = await DatabaseService.cargoStore.edit().id(req.params.id).with({
+      arrival: data.arrival,
+      departure: data.departure,
+      description: data.description,
+      images: data.images,
+      price: data.price,
+      vehicleType: data.vehicleType,
+      volume: data.volume,
+      weight: data.weight,
+    }).run()
+
+    return cargo;
   }
 }
