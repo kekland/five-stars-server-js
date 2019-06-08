72ddbbb36fad6ddad9df07b7d58882358e5b7e7b
kekland <kk.erzhan@gmail.com>
2019-06-08 18:40:14 +0600

    added alter profile request

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,9 @@
 import { RoutedController, Controller } from '../lapis_server/controller';
-import { Get } from '../lapis_server/request.methods';
+import { Get, Put } from '../lapis_server/request.methods';
 import { DatabaseService } from '../database/database.service';
 import { BadRequestException } from '../lapis_server/errors';
+import { ValidationService } from '../lapis_server/utils';
+import { ProfileEditRequestObject } from '../data/request/profile.edit.request.object';
 
 @RoutedController('/user')
 export class UserController extends Controller {
@@ -19,6 +21,27 @@ export class UserController extends Controller {
       throw new BadRequestException({ message: 'User with this username was not found.' })
     }
     else {
+      return user;
+    }
+  }
+
+  @Put('/:username')
+  async editProfile(req) {
+    const username = req.params.username
+
+    const data = await ValidationService
+      .transformAndValidate<ProfileEditRequestObject>(req.body, () => ProfileEditRequestObject)
+
+    let user = await DatabaseService.userStore.get().where((item) => item.username === username).first()
+
+    if (user == null) {
+      throw new BadRequestException({ message: 'User with this username was not found.' })
+    }
+    else {
+      await DatabaseService.userStore.edit().item(user)
+        .with({ organization: data.organization, email: data.email, name: data.name, phoneNumber: data.phoneNumber }).run();
+
+      user = await DatabaseService.userStore.get().where((item) => item.username === username).first()
       return user;
     }
   }
---
+++ src/data/request/profile.edit.request.object.ts
@@ -0,0 +1,25 @@
+import { Name } from '../../models/name';
+import * as Validator from 'class-validator'
+import { Type } from 'class-transformer';
+
+export class ProfileEditRequestObject {
+  @Validator.IsNotEmpty()
+  @Validator.IsString()
+  @Validator.IsEmail()
+  email: string;
+
+  @Validator.IsNotEmpty()
+  @Validator.IsString()
+  @Validator.IsPhoneNumber(null)
+  phoneNumber: string;
+
+  @Validator.IsNotEmpty()
+  @Validator.IsString()
+  @Validator.Length(2)
+  organization: string;
+
+  @Validator.IsNotEmpty()
+  @Validator.ValidateNested()
+  @Type(() => Name)
+  name: Name;
+}
